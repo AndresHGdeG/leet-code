0834ca8b0b44e7baad4ffd45e377823cf17a847b
AndresHGdeG <a.h.mx@outlook.com>
2023-10-10 22:52:49 -0600

    13.Roman-to-Integer
    Simple soultion with O(N) runtime

---
+++ Python3/Easy/13.Roman-to-Integer/OdN-last-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        last_number = 0
+        total = 0
+        for element in s:
+            number = roman_dic[element]            
+            if last_number < number :
+                total += (number - 2*last_number)
+            else:
+                total += number
+            last_number = number
+        return total
+
+
+test = Solution()
+test.romanToInt('MCMXCIV')
